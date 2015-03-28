9d3f905b028f85add4a2aa5846904b1a2a477242
Brandon Stiles <stilesbr1@gmail.com>
2015-03-27 18:07:10 -0700

    Changed map pointer events to not accept events

--- main.js
+++ main.js
@@ -1,39 +1,21 @@
 $(document).ready(function () {
-    var pp = $("#pushpin");
+    var DENSITY = 3;
+    var QUALITY = 2;
+    var DISTRIBUTION = 1;
 
-    var COLOR = "#26a69a";
-    pp.width(pp.width());
-    pp.pushpin({
-        top: pp.offset().top,
-        offset: 92
-    });
+    var CHART_COLOR = "#26a69a";
 
-    var options = [
-        {selector: '#density-map', offset: $('#density-map').offset().top, callback: 'expandDensities()'},
-        {selector: '#distribution-map', offset: $('#distribution-map').offset().top, callback: 'expandDistribution()'},
-        {selector: '#quality-map', offset: $('#quality-map').offset().top, callback: 'expandQualities()'}
-    ];
+    var DENSITY_SEL = '#density-map';
+    var DISTRIBUTION_SEL = '#distribution-map';
+    var QUALITY_SEL = '#quality-map';
 
-    window.expandDensities = function () {
-        $('#expand-densities').click();
-    };
-
-    window.expandDistribution = function () {
-        $('#expand-distributions').click();
-    };
-
-    window.expandQualities = function () {
-        $('#expand-qualities').click();
-    };
-
-    $('#expand-densities').click();
-
-    scrollFire(options);
-
-    $('.scrollspy').scrollSpy();
-    $(".dropdown-button").dropdown();
+    var startups = [];
+    var locations = [];
+    var cities = [];
 
     function createMap(sel, drawOption) {
+        var g;
+
         var scale = 1,
             width = $(sel).width(),
             height = 500,
@@ -58,7 +40,7 @@ $(document).ready(function () {
             .attr('height', height)
             .on('click', zoom);
 
-        var g = svg.append('g');
+        g = svg.append('g');
 
         d3.json('/data/us.json', function (error, us) {
             states = topojson.feature(us, us.objects.states).features;
@@ -79,144 +61,99 @@ $(document).ready(function () {
                 .attr('d', path);
 
             switch (drawOption) {
-                case 1:
-                    drawStartupDensityLocations(true);
+                case QUALITY:
+                    drawQuality();
                     break;
-                case 2:
-                    drawStartupDensityLocations(false);
+                case DISTRIBUTION:
+                    drawDistribution();
                     break;
-                case 3:
-                    drawStartupLocations();
+                case DENSITY:
+                    drawDensity();
                     break
             }
+
+            svg.selectAll('circle')
+                .attr('class', 'overlay');
         });
 
-
-        function removeLocations() {
-            svg.selectAll('circle').remove();
+        function drawDistribution() {
+            svg.selectAll('circle')
+                .data(locations).enter()
+                .append('circle')
+                .attr('cx', function (d) {
+                    return projection(d.coords)[0] * scale;
+                })
+                .attr('cy', function (d) {
+                    return projection(d.coords)[1] * scale;
+                })
+                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + scale + ")translate(" + -x + "," + -y + ")")
+                .attr('r', '2px')
+                .attr('fill', CHART_COLOR)
+                .on('mouseover', function (d) {
+                    console.log('yo')
+                })
         }
 
-        function drawStartupLocations() {
-            removeLocations();
-
-            d3.json('/data/startups.json', function (error, startups) {
-                var scaleFactor = scale == 4 ? 6 : 1;
-                var locations = [];
-                var count = 0;
-
-                for (var startup in startups) {
-                    for (var i = 0; i < startups[startup].locations.length; i++) {
-                        locations.push([
-                            parseFloat(startups[startup].locations[i].location[0]),
-                            parseFloat(startups[startup].locations[i].location[1])
-                        ]);
-                    }
-
-                    count++;
-                }
-
-                $("#startup-badge").html(count);
-
-                svg.selectAll('circle')
-                    .data(locations).enter()
-                    .append('circle')
-                    .attr('cx', function (d) {
-                        return projection(d)[0] * scaleFactor;
-                    })
-                    .attr('cy', function (d) {
-                        return projection(d)[1] * scaleFactor;
-                    })
-                    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + scale + ")translate(" + -x + "," + -y + ")")
-                    .attr('r', '2px')
-                    .attr('fill', COLOR);
-            });
+        function drawDensity() {
+            svg.selectAll('circle')
+                .data(cities).enter()
+                .append('circle')
+                .attr('cx', function (d) {
+                    return projection([d.latitude, d.longitude])[0] * scale;
+                })
+                .attr('cy', function (d) {
+                    return projection([d.latitude, d.longitude])[1] * scale;
+                })
+                .attr('r', function (d) {
+                    var size = Math.sqrt(d.count) * 5;
+                    return size + 'px';
+                })
+                .attr('fill', function () {
+                    return CHART_COLOR;
+                })
+                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + scale + ")translate(" + -x + "," + -y + ")")
+                .attr('opacity', function () {
+                    return '0.3';
+                })
+                .on('mouseover', function (d) {
+                    console.log('yo')
+                })
+                console.log('good')
         }
 
-        function drawStartupDensityLocations(quality) {
-            quality = quality || false;
+        function drawQuality() {
+            var colorScale = d3.scale.linear()
+                .domain([1, 7])
+                .range(["white", CHART_COLOR]);
 
-            removeLocations();
-
-            d3.json('/data/startups.json', function (error, startups) {
-                var scaleFactor = 1;
-                var cities = [];
-                var sentinel;
-                var count = 0;
-
-                for (var startup in startups) {
-                    sentinel = true;
-
-                    for (var i = 0; i < startups[startup].locations.length; i++) {
-                        for (var j = 0; j < cities.length; j++) {
-                            if (cities[j].name == startups[startup].locations[i].name) {
-                                cities[j].count++;
-                                cities[j].quality = (cities[j].quality + startups[startup].quality) / 2;
-                                sentinel = false;
-                                break;
-                            }
-                        }
-
-                        if (sentinel) {
-                            cities.push({
-                                count: 1,
-                                quality: startups[startup].quality,
-                                name: startups[startup].locations[i].name,
-                                latitude: parseFloat(startups[startup].locations[i].location[0]),
-                                longitude: parseFloat(startups[startup].locations[i].location[1])
-                            });
-                        }
-                    }
-
-                    count++;
-                }
-
-                $("#startup-badge").html(count);
-
-
-                var colorScale = d3.scale.linear()
-                    .domain([1, 7])
-                    .range(["white", COLOR]);
-
-                svg.selectAll('circle')
-                    .data(cities).enter()
-                    .append('circle')
-                    .attr('cx', function (d) {
-                        return projection([d.latitude, d.longitude])[0] * scaleFactor;
-                    })
-                    .attr('cy', function (d) {
-                        return projection([d.latitude, d.longitude])[1] * scaleFactor;
-                    });
-
-                svg.selectAll('circle')
-                    .attr('r', function (d) {
-                        if (quality) {
-                            return '5px';
-                        } else {
-                            var size = Math.sqrt(d.count) * 5;
-                            return size + 'px';
-                        }
-                    })
-                    .attr('fill', function (d) {
-                        if (quality) {
-                            return colorScale(d.quality);
-                        } else {
-                            return COLOR;
-                        }
-                    })
-                    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + scale + ")translate(" + -x + "," + -y + ")")
-                    .attr('opacity', function () {
-                        if (quality) {
-                            return '0.9';
-                        } else {
-                            return '0.3';
-                        }
-                    });
-            });
+            svg.selectAll('circle')
+                .data(cities).enter()
+                .append('circle')
+                .attr('cx', function (d) {
+                    return projection([d.latitude, d.longitude])[0] * scale;
+                })
+                .attr('cy', function (d) {
+                    return projection([d.latitude, d.longitude])[1] * scale;
+                })
+                .attr('r', function () {
+                    return '5px';
+                })
+                .attr('fill', function (d) {
+                    return colorScale(d.quality);
+                })
+                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + scale + ")translate(" + -x + "," + -y + ")")
+                .attr('opacity', function () {
+                    return '0.9';
+                })
+                .on('mouseover', function (d) {
+                    console.log('yo')
+                })
         }
 
         function zoom(d) {
             if (d && centered !== d) {
                 var centroid = path.centroid(d);
+
                 x = centroid[0];
                 y = centroid[1];
                 scale = 4;
@@ -245,7 +182,111 @@ $(document).ready(function () {
         }
     }
 
-    createMap('#distribution-map', 3);
-    createMap('#density-map', 2);
-    createMap('#quality-map', 1);
+    function parseLocations() {
+        var count = 0;
+
+        locations = [];
+
+        for (var startup in startups) {
+            for (var i = 0; i < startups[startup].locations.length; i++) {
+                locations.push({
+                    name: startups[startup].name,
+                    coords: [
+                        parseFloat(startups[startup].locations[i].location[0]),
+                        parseFloat(startups[startup].locations[i].location[1])
+                    ]
+                });
+            }
+
+            count++;
+        }
+    }
+
+    function parseCities() {
+        var sentinel;
+        var count = 0;
+
+        for (var startup in startups) {
+            sentinel = true;
+
+            for (var i = 0; i < startups[startup].locations.length; i++) {
+                for (var j = 0; j < cities.length; j++) {
+                    if (cities[j].name == startups[startup].locations[i].name) {
+                        cities[j].count++;
+                        cities[j].quality = (cities[j].quality + startups[startup].quality) / 2;
+                        sentinel = false;
+                        break;
+                    }
+                }
+
+                if (sentinel) {
+                    cities.push({
+                        count: 1,
+                        quality: startups[startup].quality,
+                        name: startups[startup].locations[i].name,
+                        startup: startups[startup],
+                        latitude: parseFloat(startups[startup].locations[i].location[0]),
+                        longitude: parseFloat(startups[startup].locations[i].location[1])
+                    });
+                }
+            }
+
+            count++;
+        }
+    }
+
+    function fetchStartups(callback) {
+        d3.json('/data/startups.json', function (error, data) {
+            startups = data;
+
+            parseCities();
+            parseLocations();
+
+            callback();
+        });
+    }
+
+    function createMaps() {
+        createMap(DENSITY_SEL, DENSITY);
+        createMap(DISTRIBUTION_SEL, DISTRIBUTION);
+        createMap(QUALITY_SEL, QUALITY);
+    }
+
+    function initMaterialize() {
+        var pp = $("#pushpin");
+
+        pp.width(pp.width());
+        pp.pushpin({
+            top: pp.offset().top,
+            offset: 92
+        });
+
+        var options = [
+            {selector: '#density-map', offset: $('#density-map').offset().top, callback: 'expandDensities()'},
+            {selector: '#distribution-map', offset: $('#distribution-map').offset().top, callback: 'expandDistribution()'},
+            {selector: '#quality-map', offset: $('#quality-map').offset().top, callback: 'expandQualities()'}
+        ];
+
+        window.expandDensities = function () {
+            $('#expand-densities').click();
+        };
+
+        window.expandDistribution = function () {
+            $('#expand-distributions').click();
+        };
+
+        window.expandQualities = function () {
+            $('#expand-qualities').click();
+        };
+
+        $('#expand-densities').click();
+
+        scrollFire(options);
+
+        $('.scrollspy').scrollSpy();
+        $(".dropdown-button").dropdown();
+    }
+
+    fetchStartups(createMaps);
+    initMaterialize();
 });
